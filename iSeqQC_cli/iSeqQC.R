e1acd51c5aae415ba97d99b9fb45379d00e400c5
Gaurav Kumar <gaurav.neuro@gmail.com>
2020-04-17 16:21:27 -0400

    revised

--- iSeqQC_cli/iSeqQC.R
+++ iSeqQC_cli/iSeqQC.R
@@ -48,11 +48,11 @@ tpmfiltering_data <- function(DF1, DF2){
 }
 
 SumMarize<- function(DF){
-  sm <- as.data.frame(describe(DF, ranges=TRUE))
+  sm <- as.data.frame(describe(DF, ranges=TRUE, fast = FALSE))
   sm=setDT(sm, keep.rownames = T)[]
   sm= sm[,-c(2,6,7,13)]
-  addsum= data.frame(colSums(DF[,-1]))
-  addsum_zero= data.frame(colSums(DF[,-1]>0))
+  addsum= data.frame(colSums(DF))
+  addsum_zero= data.frame(colSums(DF>0))
   newsm= cbind(sm,addsum, addsum_zero)
   colnames(newsm) <- c("Samples", "Detected Genes", "Mean", "SD", "Median", "Min", "Max", "Range", "Skew", "Kurtosis", "Library Size", "Expressed Genes")
   write.table(newsm, file="Summary.Statistics.sample.txt", sep='\t', row.names=F)
